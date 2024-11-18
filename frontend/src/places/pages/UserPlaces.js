a0191acc120e587a0dab336b0f1a7514889bd676
clayton-cunningham <clayton.c.cunningham@gmail.com>
2024-11-18 15:19:45 -0500

    Authentication

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -15,20 +15,20 @@ const DUMMY_PLACES = [
       lat: 40.7484405,
       lng: -73.9878584
     },
-    creator: 'u1'
+    creator: '1'
   },
   {
     id: 'p2',
-    title: 'Emp. State Building',
+    title: 'Emp. State Building 2',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+      '../001.png',
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
       lng: -73.9878584
     },
-    creator: 'u2'
+    creator: '2'
   }
 ];
 
