1956a0bb11a868f108ea52d843cb7a3346e1d5dd
clayton-cunningham <clayton.c.cunningham@gmail.com>
2024-12-07 13:01:06 -0500

    New: persistent token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,22 +13,51 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+
+let logOutTimer = "";
 
 function App() {
 
   const [token, setToken] = useState();
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState();
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expiration) => {
     setToken(token);
     setUserId(uid);
+
+    const tokenExpirationDate = new Date(expiration || new Date().getTime() + (1000 * 60 * 60));
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem('userData', JSON.stringify({
+      userId: uid,
+      token: token,
+      expiration: tokenExpirationDate.toISOString(),
+    }))
   }, [])
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
+      login(storedData.uid, storedData.token, storedData.expiration);
+    }
+  }, [login]);
   
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem('userData');
   }, [])
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remaningTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logOutTimer = setTimeout(logout, remaningTime);
+    } else {
+      clearTimeout(logOutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
 
   let routes;
 
