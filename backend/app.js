564be2350239eed5c8fc9010437ac3ac82221d78
clayton-cunningham <clayton.c.cunningham@gmail.com>
2024-12-09 16:39:22 -0500

    New: frontend env

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@fullstackcluster.wapcj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=FullstackCluster`)
     .then(() => {
-        app.listen(5000);
+        app.listen(process.env.PORT || 5000);
     })
     .catch(err => {
         console.log(err);
